a140db537460d6808e0a7f6fec47f2611a0e2168
singhh22 <singhh22@medtronic.com>
2023-07-14 14:16:20 +0530

    Connecting the crimeDetail views within the CrimeDetailFragment

---
+++ app/src/main/java/com/example/criminalintent/CrimeDetailFragment.kt
@@ -0,0 +1,74 @@
+package com.example.criminalintent
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
+import androidx.fragment.app.Fragment
+import com.example.criminalintent.databinding.FragmentCrimeDetailBinding
+import java.util.Date
+import java.util.UUID
+
+/**
+ * CrimeDetailFragment is responsible for displaying the details of a specific crime.
+ */
+class CrimeDetailFragment : Fragment() {
+    private lateinit var crime: Crime
+    private lateinit var binding: FragmentCrimeDetailBinding
+
+    /**
+     * Initializes the crime object.
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        crime = Crime(
+            id = UUID.randomUUID(),
+            title = "",
+            date = Date(),
+            isSolved = false
+        )
+    }
+
+    /**
+     * Inflates the fragment's layout which includes all the necessary views
+     * to display the crime details.
+     */
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Android system manages time to add fragment's view to container.
+        // Activity provides the place. So passed false, to let android system manage it.
+        binding = FragmentCrimeDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    /**
+     * Once the view is created, this function wires up the views
+     * with data provided by the user.
+     */
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            // Setting listeners to crime title box
+            crimeTitle.doOnTextChanged { text, _, _, _ ->
+                crime = crime.copy(title = text.toString())
+            }
+
+            // Setting text to date button
+            crimeDate.apply {
+                text = crime.date.toString()
+                isEnabled = false
+            }
+
+            // Setting listener to crime solved checkbox
+            crimeSolved.setOnCheckedChangeListener { _, isChecked ->
+                crime = crime.copy(isSolved = isChecked)
+            }
+
+        }
+    }
+}
