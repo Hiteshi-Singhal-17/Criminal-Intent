a1c04cbf0e73bf574342cd349cb2829f43bbb664
singhh22 <singhh22@medtronic.com>
2023-07-17 16:13:58 +0530

    Implement repeatOnLifecycle() to simplify job management and lifecycle methods handling. Ensure view setup is confined to onViewCreated()

--- app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
@@ -1,39 +1,29 @@
 package com.example.criminalintent
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.criminalintent.databinding.FragmentCrimeListBinding
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
-private const val TAG = "CrimeListFragment"
 
 class CrimeListFragment : Fragment() {
     // Associating fragment with the view model
     private val crimeListViewModel: CrimeListViewModel by viewModels()
     private var _binding: FragmentCrimeListBinding? = null
-    private var job: Job? = null
 
     // Null out references to views in onDestroyView()
     private val binding
-        get() = checkNotNull(_binding){
+        get() = checkNotNull(_binding) {
             "Cannot access binding because it is null. Is the view visible?"
         }
-
-    /**
-     * Logs the number of crimes.
-     */
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
-    }
 
     /**
      * Inflates the fragment's layout which includes all the necessary views
@@ -44,29 +34,34 @@ class CrimeListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentCrimeListBinding.inflate(inflater,container,false)
-        // Setting up linear layout manager for recycler view
-        binding.crimeRecyclerView.layoutManager = LinearLayoutManager(context)
-        // Retrieving the list of crimes from the ViewModel
-        val crimes = crimeListViewModel.crimes
-        // Set the adapter for the recycler view
-        binding.crimeRecyclerView.adapter = CrimeListAdapter(crimes)
-
+        _binding = FragmentCrimeListBinding.inflate(inflater, container, false)
         return binding.root
     }
 
-    override fun onStart() {
-        super.onStart()
-        job = viewLifecycleOwner.lifecycleScope.launch {
-            val crimes = crimeListViewModel.loadCrimes()
-            binding.crimeRecyclerView.adapter = CrimeListAdapter(crimes)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        /* Setting up linear layout manager for recycler view
+         It's generally recommended to set up the layout manager
+         and adapter of a RecyclerView in the onViewCreated() method.
+         This sequence ensures that the View hierarchy is fully set up before trying to access
+         and modify it
+         */
+        binding.crimeRecyclerView.layoutManager = LinearLayoutManager(context)
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            // repeatOnLifecycle -> suspending function requires coroutine scope to be launched,
+            // won't cancel coroutines, cancels the job when goes to the Destroyed state.
+            // Coroutine will be cancelled by coroutine scope only, in this case
+            // viewLifecycleOwner; tied to the lifecycle of fragment's view.
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                // Retrieving the list of crimes from the ViewModel
+                val crimes = crimeListViewModel.loadCrimes()
+                // Set the adapter for the recycler view
+                binding.crimeRecyclerView.adapter = CrimeListAdapter(crimes)
+            }
         }
     }
 
-    override fun onStop() {
-        super.onStop()
-        job?.cancel()
-    }
     /**
      * Destroy the view hierarchy and set null to binding object.
      */
