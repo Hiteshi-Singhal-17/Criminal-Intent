284cdfb2dcbf5d1f3a0a13970c68b263ec46cf6b
singhh22 <singhh22@medtronic.com>
2023-07-17 17:18:20 +0530

    Configure project to use Room and define database entity.

--- app/src/main/java/com/example/criminalintent/Crime.kt
+++ app/src/main/java/com/example/criminalintent/Crime.kt
@@ -1,13 +1,20 @@
 package com.example.criminalintent
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.Date
 import java.util.UUID
 
 /**
  * An instance of crime will represent a single office crime
  */
+// Room will create database table for the entity, here, crime
+// Each row will represent a crime
+// Columns will be property defined in the crime data class.
+// id is primary key to uniquely identify each crime.
+@Entity
 data class Crime(
-    val id: UUID,
+    @PrimaryKey val id: UUID,
     val title: String,
     val date: Date,
     val isSolved: Boolean
