90147c846b93e0e210e568013e471f796224b83c
singhh22 <singhh22@medtronic.com>
2023-08-31 17:32:45 +0530

    Passing date from DatePickerFragment to CrimeDetailFragment so the date of crime shows updated crime date.

--- app/src/main/java/com/example/criminalintent/DatePickerFragment.kt
+++ app/src/main/java/com/example/criminalintent/DatePickerFragment.kt
@@ -3,9 +3,12 @@ package com.example.criminalintent
 import android.app.DatePickerDialog
 import android.app.Dialog
 import android.os.Bundle
+import androidx.core.os.bundleOf
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.setFragmentResult
 import androidx.navigation.fragment.navArgs
 import java.util.Calendar
+import java.util.GregorianCalendar
 
 /*
 If datePickerDialog is wrapped in DialogFragment,
@@ -22,6 +25,11 @@ class DatePickerFragment : DialogFragment() {
     private val args: DatePickerFragmentArgs by navArgs()
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dateListener = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+            val resultDate = GregorianCalendar(year, month, dayOfMonth).time
+            setFragmentResult(REQUEST_KEY_DATE, bundleOf(BUNDLE_KEY_DATE to resultDate))
+        }
+
         val calendar = Calendar.getInstance()
         // To fetch day, month and year from date timestamp
         calendar.time = args.crimeDate
@@ -32,11 +40,16 @@ class DatePickerFragment : DialogFragment() {
         // Creating an instance of DatePickerDialog.
         return DatePickerDialog(
             requireContext(), // to access necessary resources like theme and strings
-            null,      // DateListener
+            dateListener,
             initialYear,
             initialMonth,
             initialDay
         )
+    }
+
+    companion object {
+        const val REQUEST_KEY_DATE = "REQUEST_KEY_DATE"
+        const val BUNDLE_KEY_DATE = "BUNDLE_KEY_DATE"
 
     }
 }
